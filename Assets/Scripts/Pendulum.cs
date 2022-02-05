4c7c23d9bb5cd4dccc2f67c120d9b8b8703ee5c8
anko0116 <hyukahn@umich.edu>
2022-02-05 12:51:05 -0500

    Add Pendulum - but doesn't slide across ground

---
+++ Assets/Scripts/Pendulum.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    public Camera mainCamera;
+    public LineRenderer lineRenderer;
+    public DistanceJoint2D distanceJoint;
+    void Start()
+    {
+        distanceJoint.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            // Create visual line from clicked mouse position to the Player object
+            lineRenderer.SetPosition(0, mousePos);
+            lineRenderer.SetPosition(1, gameObject.transform.position);
+            lineRenderer.enabled = true;
+            // Create physical joint from Player object to the mouse position
+            distanceJoint.connectedAnchor = mousePos; 
+            distanceJoint.enabled = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0)){
+            lineRenderer.enabled = false;
+            distanceJoint.enabled = false;
+        }
+
+        if (distanceJoint.enabled) {
+            lineRenderer.SetPosition(1, gameObject.transform.position);
+        }
+    }
+}
